4989cdf797c4877c88fbe75f871eea89fdd32202
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-03-31 15:32:07 -0400

    Fixed the if/else conditions for calling the next step. Removed the multithreading. Adjusted the names of various temporary files. Removed the -L argument from GATK as it is not necessary as this is a read walker - it just means that GATK cannot accurately predict the total time the job will take

--- ExmAln.6.ApplyRecalibration.sh
+++ ExmAln.6.ApplyRecalibration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#$ -cwd -pe smp 6 -l mem=2G,time=6:: -N AppRcl
+#$ -cwd -l mem=12G,time=2:: -N AppRcl
 
 
-#This script takes a bam file and uses a previously generated base quality score recalibration (BQSR) table to recalibrate them using GATK. If the bam file has previously been split into chromosomes (default 24, i.e. 1-22, X, Y) a list can be provided, ensuring each the files are listed in chromosome order (1-22 then X then Y). The filename of list MUST end ".list"
+#This script takes a bam file and uses a previously generated base quality score recalibration (BQSR) table to recalibrate them using GATK. If the bam file has previously been split into chromosomes (default 24, i.e. 1-22, X, Y) a list can be provided. The filename of list MUST end ".list"
 #	InpFil - (required) - Path to Bam file or a list of BamFiles to be recalibrated
 #	RclTab - (required) - Previously generated BQSR table
 #	RefFiles - (required) - shell file to export variables with locations of reference files, jar files, and resource directories; see list below
@@ -33,7 +33,7 @@
 usage="
 ExmAln.6.ApplyRecalibration.sh -i <InputFile> -x <GATK BQSR table> -r <reference_file> -t <targetfile> -l <logfile> -PABH
 
-	 -i (required) - Path to Bam file to be aligned or \".list\" file containing a multiple paths
+	 -i (required) - Path to Bam file to be recalibrated or \".list\" file containing a multiple paths
 	 -x (required) - Previously generated BQSR table
 	 -r (required) - shell file to export variables with locations of reference files and resource directories
 	 -t (required) - Exome capture kit targets bed file (must end .bed for GATK compatability)
@@ -70,35 +70,23 @@ source $RefFil
 source $EXOMPPLN/exome.lib.sh #library functions begin "func"
 
 #Set Local Variables
-Chr=$SGE_TASK_ID
-Chr=${Chr/23/X}
-Chr=${Chr/24/Y}
-if [[ "$BUILD" = "hg19" ]]; then
-	Chr=chr$Chr
-fi
-RalList=$InpFil #keep the reference to the list file if the input is a list so that it can be modified and passed along the pipeline if necessary
+InpNam=`basename ${InpFil/.bam/}`
+InpNam=`basename ${InpFil/.list/}`
+RclLst=Recalibrated.$InpNam.list #File listing paths to recalibrated bams
+RclDir=$InpNam.recalibrated ; mkdir -p $RclDir # a directory to collect all of the recalibrated files in 
 funcFilfromList #if the input is a list get the appropriate input file for this job of the array --> $InpFil
 BamFil=`readlink -f $InpFil` #resolve absolute path to bam
 BamNam=`basename ${BamFil/.bam/}` #a name to use for the various files
-TmpTar=$BamNam.TmpTarFil.$Chr.bed #temporary target file
 if [[ -z $LogFil ]];then LogFil=$BamNam.appBQSR.log; fi # a name for the log file
 RclDir=$BamNam.recalibrated # a directory to collect all of the recalibrated files in 
-mkdir -p $RclDir
 RclFil=$RclDir/$BamNam.recalibrated.bam #file to output recalibrated bam to
-RclLst=Recalibrated.$BamNam.list #File listing paths to recalibrated bams
-GatkLog=$BamNam.AppBQSR.$Chr.gatklog #a log for GATK to output to, this is then trimmed and added to the script log
-TmpLog=$BamNam.AppBQSR.$Chr.temp.log #temporary log file
-TmpDir=$BamNam.AppBQSR.$Chr.tempdir; mkdir -p $TmpDir #temporary directory
+GatkLog=$BamNam.AppBQSR.gatklog #a log for GATK to output to, this is then trimmed and added to the script log
+TmpLog=$BamNam.AppBQSR.temp.log #temporary log file
+TmpDir=$BamNam.AppBQSR.tempdir; mkdir -p $TmpDir #temporary directory
 
 #Start Log
 ProcessName="Recalibrate Base Quality Scores with GATK" # Description of the script - used in log
 funcWriteStartLog
-
-#Make chromosome specific exome target file
-echo "CHR "$Chr >> $TmpLog
-echo "Target File: "$TgtBed >> $TmpLog
-grep -E "^$Chr[[:blank:]]" $TgtBed > $TmpTar
-echo "Start: "$(head -n 1 $TmpTar)" Finish: "$(tail -n 1 $TmpTar) >> $TmpLog
 
 #Apply Recalibration
 StepName="Apply recalibration using GATK PrintReads" # Description of this step - used in log
@@ -106,10 +94,8 @@ StepCmd="java -Xmx7G -Djava.io.tmpdir=$TmpDir -jar $GATKJAR
  -T PrintReads 
  -R $REF 
  -I $BamFil 
- -L $TmpTar 
  -BQSR $RclTab 
  -o $RclFil 
- -nct 6
  --filter_mismatching_base_and_quals
  -log $GatkLog" #command to be run
 funcGatkAddArguments
@@ -118,14 +104,16 @@ funcRunStep
 #generate realigned file list
 find `pwd` | grep -E bam$ | grep $RclDir | sort -V > $RclLst
 
-#Call next step; if the original input was a list then only first job of the array calls this and puts it on hold until entire array has finished
+#Call next step
+#if the original input was a list then only first job of the array calls the script to merge all the bams and puts it on hold until entire array has finished
+#if there was only one bam supplied the depth of coverage and quality distribution scripts are called
 if [[ $SGE_TASK_ID -eq 1 ]]; then
    NextJob="Merge Recalibrated Bams"
    QsubCmd="qsub -hold_jid $JOB_ID -o stdostde/ -e stdostde/ $EXOMPPLN/ExmAln.7.MergeBams.sh -i $RclLst -r $RefFil -t $TgtBed -l $LogFil -P"
    if [[ $AllowMisencoded == "true" ]]; then QsubCmd=$QsubCmd" -A"; fi
    if [[ $BadET == "true" ]]; then QsubCmd=$QsubCmd" -B"; fi
    funcPipeLine
-else
+elif [[ -z $SGE_TASK_ID ]]; then
    NextJob="Get Depth of Coverage Statistics"
    QsubCmd="qsub -o stdostde/ -e stdostde/ $EXOMPPLN/ExmAln.8a.DepthofCoverage.sh -i $RclLst -r $RefFil -t $TgtBed -l $LogFil"
    if [[ $AllowMisencoded == "true" ]]; then QsubCmd=$QsubCmd" -A"; fi

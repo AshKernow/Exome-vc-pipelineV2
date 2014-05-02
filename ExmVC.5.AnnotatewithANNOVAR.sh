a5c0855b36865fd90371ae9f6ce86165814bb63b
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-05-02 17:09:01 -0400

    Changed the ID names for the annotations to be added to the INFO field

--- ExmVC.5.AnnotatewithANNOVAR.sh
+++ ExmVC.5.AnnotatewithANNOVAR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -cwd -l mem=4G,time=2:: -N AnnVC
+#$ -cwd -l mem=8G,time=2:: -N AnnVC
 
 #This script takes a bam file or a list of bam files (filename must end ".list") and runs variant calling using the HaplotypeCaller in gVCF mode
 #	InpFil - (required) - Path to Bam file to be aligned. Alternatively a file with a list of bams can be provided and the script run as an array job. List file name must end ".list"
@@ -74,8 +74,9 @@ StepCmd="table_annovar23.pl $TmpVar $ANNHDB --buildver hg19 --remove -protocol r
 funcRunStep
 AnnFil=$AnnFil.hg19_multianno.txt
 
-##sort, replace spaces, zip and index
-#regarding spaces - annovar output has these in the refgene function, e.g. "synonymous SNV", but they are not permitted in vcf format and other tools (e.g. GATK) will throw an error if they encounter them
+##sort, replace spaces and semi-colons, zip and index
+# - annovar output has spaces in the RefSeq function code, e.g. "synonymous SNV", but they are not permitted in vcf format and other tools (e.g. GATK) will throw an error if they encounter them
+# - annovar separates multiple gene names in the RefSeq gene name field with semi-colons, this causes and error in the vcf
 head -n 1 $AnnFil > $AnnFil.tempheader
 tail -n+2 $AnnFil | sort -V | awk '{gsub( / /, ""); print}' | awk '{gsub( /;/, ","); print}' >> $AnnFil.tempheader
 mv $AnnFil.tempheader $AnnFil
@@ -85,23 +86,23 @@ tabix  -s 1 -b 2 -e 3 $AnnFil.gz
 #Annotate with vcftools
 StepNam="Annotate with vcftools"
 StepCmd="cat $InpFil | vcf-annotate -a $AnnFil.gz
- -c -,-,-,-,-,INFO/Func.refGene,INFO/Gene.refGene,INFO/ExonicFunc.refGene,INFO/AAChange.refGene,INFO/esp6500si_all,-,-,INFO/1000g2012apr_all,-,-,-,-,INFO/LJB23_SIFT_score,-,INFO/LJB23_SIFT_pred,-,-,INFO/LJB23_Polyphen2_HVAR_score,INFO/LJB23_Polyphen2_HVAR_pred,-,-,-,INFO/LJB23_MutationTaster_score,-,INFO/LJB23_MutationTaster_pred,-,-,-,-,-,-,-,-,-,-,-,INFO/LJB23_GERP++,INFO/LJB23_PhyloP,INFO/LJB23_SiPhy,INFO/caddgt,CHROM,POS,-,REF,ALT
- -d key=INFO,ID=Func.refGene,Number=1,Type=String,Description='function'
- -d key=INFO,ID=Gene.refGene,Number=1,Type=String,Description='refGene GeneName'
- -d key=INFO,ID=ExonicFunc.refGene,Number=1,Type=String,Description='functional Class'
- -d key=INFO,ID=AAChange.refGene,Number=1,Type=String,Description='Amino Acid change'
- -d key=INFO,ID=esp6500si_all,Number=1,Type=Float,Description='ESP6500 alternative allele frequency'
- -d key=INFO,ID=1000g2012apr_all,Number=1,Type=Float,Description='1000 genome alternative allele frequency'
- -d key=INFO,ID=LJB23_SIFT_score,Number=1,Type=Float,Description='SIFT score'
- -d key=INFO,ID=LJB23_SIFT_pred,Number=1,Type=String,Description='SIFT Prediction'
- -d key=INFO,ID=LJB23_Polyphen2_HVAR_score,Number=1,Type=Float,Description='PolyPhen2 HVAR Score'
- -d key=INFO,ID=LJB23_Polyphen2_HVAR_pred,Number=1,Type=Character,Description='PolyPhen2 HVAR Prediction'
- -d key=INFO,ID=LJB23_MutationTaster_score,Number=1,Type=Float,Description='MutationTaster Score'
- -d key=INFO,ID=LJB23_MutationTaster_pred,Number=1,Type=Character,Description='MutationTaster Prediction'
- -d key=INFO,ID=LJB23_GERP++,Number=1,Type=Float,Description='GERP++ score'
- -d key=INFO,ID=LJB23_PhyloP,Number=1,Type=Float,Description='PhyloP score'
- -d key=INFO,ID=LJB23_SiPhy,Number=1,Type=Float,Description='SiPhy scores'
- -d key=INFO,ID=caddgt,Number=1,Type=Float,Description='Whole-genome CADD score'
+ -c -,-,-,-,-,INFO/SeqFunc,INFO/GeneName,INFO/MutClass,INFO/AAChange,INFO/ESPfreq,-,-,INFO/1KGfreq,-,-,-,-,INFO/SIFTscr,-,INFO/SIFTprd,-,-,INFO/PP2scr,INFO/PP2prd,-,-,-,INFO/MutTscr,-,INFO/MutTprd,-,-,-,-,-,-,-,-,-,-,-,INFO/GERP,INFO/PhyloP,INFO/SiPhy,INFO/CADD,CHROM,POS,-,REF,ALT
+ -d key=INFO,ID=SeqFunc,Number=1,Type=String,Description='Genomic region/Sequence Function'
+ -d key=INFO,ID=GeneName,Number=1,Type=String,Description='refGene GeneName'
+ -d key=INFO,ID=MutClass,Number=1,Type=String,Description='Mutational Class'
+ -d key=INFO,ID=AAChange,Number=1,Type=String,Description='Amino Acid change'
+ -d key=INFO,ID=ESPfreq,Number=1,Type=Float,Description='Exome Sequencing Project 6500 alternative allele frequency'
+ -d key=INFO,ID=1KGfreq,Number=1,Type=Float,Description='1000 genome alternative allele frequency'
+ -d key=INFO,ID=SIFTscr,Number=1,Type=Float,Description='SIFT score'
+ -d key=INFO,ID=SIFTprd,Number=1,Type=String,Description='SIFT prediction'
+ -d key=INFO,ID=PP2scr,Number=1,Type=Float,Description='PolyPhen2 HVAR score'
+ -d key=INFO,ID=PP2prd,Number=1,Type=Character,Description='PolyPhen2 HVAR prediction'
+ -d key=INFO,ID=MutTscr,Number=1,Type=Float,Description='MutationTaster score'
+ -d key=INFO,ID=MutTprd,Number=1,Type=Character,Description='MutationTaster prediction'
+ -d key=INFO,ID=GERP,Number=1,Type=Float,Description='GERP++ score'
+ -d key=INFO,ID=PhyloP,Number=1,Type=Float,Description='PhyloP score'
+ -d key=INFO,ID=SiPhy,Number=1,Type=Float,Description='SiPhy scores'
+ -d key=INFO,ID=CADD,Number=1,Type=Float,Description='Whole-genome CADD score'
  > $InpFil.annotated.vcf"
 funcRunStep
 

73c05f03ee4f84054dfe40170ea7517eca81d3f6
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-08-22 14:52:01 -0400

    Move gvcf creation to end of alignment pipeline

--- ExmAln.6.HaplotypeCaller_GVCFmode.sh
+++ ExmAln.6.HaplotypeCaller_GVCFmode.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#$ -cwd  -l mem=12G,time=10:: -N HCgVCF
+#$ -cwd  -l mem=12G,time=16:: -N HCgVCF
 
 #This script takes a bam file or a list of bam files (filename must end ".list") and runs variant calling using the HaplotypeCaller in gVCF mode
 #    InpFil - (required) - Path to Bam file to be aligned. Alternatively a file with a list of bams can be provided and the script run as an array job. List file name must end ".list"
 #    RefFil - (required) - shell file containing variables with locations of reference files, jar files, and resource directories; see list below for required variables
 #    TgtBed - (optional) - Exome capture kit targets bed file (must end .bed for GATK compatability) ; may be specified using a code corresponding to a variable in the RefFil giving the path to the target file- only required if calling pipeline
-#    VcfNam - (optional) - A name for the analysis - to be used for naming output files. Will be derived from input filename if not provided; only used if calling pipeline
 #    LogFil - (optional) - File for logging progress
-#    Flag - P - PipeLine - call the next step in the pipeline at the end of the job
 #    Flag - B - BadET - prevent GATK from phoning home
 #    Help - H - (flag) - get usage information
 
@@ -29,31 +27,24 @@
 
 #set default arguments
 usage="
-ExmVC.1.HaplotypeCaller_GVCFmode.sh -i <InputFile> -r <reference_file> -t <targetfile> -l <logfile> -PABH
+(-t <X>-<Y> [if providing a list]) ExmVC.1.HaplotypeCaller_GVCFmode.sh -i <InputFile> -r <reference_file> -t <targetfile> -l <logfile> -PABH
 
      -i (required) - Path to Bam file for variant calling or \".list\" file containing a multiple paths
      -r (required) - shell file containing variables with locations of reference files and resource directories
      -t (required) - Exome capture kit targets or other genomic intervals bed file (must end .bed for GATK compatability)
-     -n (optional) - Analysis/output VCF name - will be derived from input filename if not provided; only used if calling pipeline
      -l (optional) - Log file
-     -P (flag) - Call next step of exome analysis pipeline after completion of script
      -B (flag) - Prevent GATK from phoning home
      -H (flag) - echo this message and exit
 "
 
-AllowMisencoded="false"
-PipeLine="false"
 BadET="false"
 
-PipeLine="false"
 while getopts i:r:n:l:t:PBH opt; do
     case "$opt" in
         i) InpFil="$OPTARG";;
         r) RefFil="$OPTARG";; 
-        n) VcfNam="$OPTARG";;
         l) LogFil="$OPTARG";;
-        t) TgtBed="$OPTARG";; 
-        P) PipeLine="true";;
+        t) TgtBed="$OPTARG";;
         B) BadET="true";;
         H) echo "$usage"; exit;;
   esac
@@ -72,12 +63,6 @@ source $EXOMPPLN/exome.lib.sh #library functions begin "func" #library functions
 
 #Set local Variables
 funcGetTargetFile #If the target file has been specified using a code, get the full path from the exported variable
-if [[ "$ArrNum" != "undefined" ]]; then 
-    if [[ -z "$VcfNam" ]];then VcfNam=`basename $InpFil`; VcfNam=${VcfNam%%.*}; fi  # a name for the output files
-    VcfLst=HCgVCF.$VcfNam.list #File listing paths to gVCF
-    if [[ -z $LogFil ]]; then LogFil=$VcfNam.HCgVCF.log; fi # a name for the log file
-    if [[ "$PipeLine" == "true" ]]; then awk '{ gsub(/.*\//, ""); gsub(/.bam$/, ".g.vcf"); print }' $InpFil > $VcfLst; fi #make a list of the output gVCF files for passing to next step of pipeline
-fi
 ArrNum=$SGE_TASK_ID
 funcFilfromList #if the input is a list get the appropriate input file for this job of the array --> $InpFil
 BamFil=`readlink -f $InpFil` #resolve absolute path to bam
@@ -117,13 +102,5 @@ StepCmd="java -Xmx7G -Djava.io.tmpdir=$TmpDir -jar $GATKJAR
 funcGatkAddArguments # Adds additional parameters to the GATK command depending on flags (e.g. -B or -F)
 funcRunStep
 
-#Call next step
-if [[ "$ArrNum" -eq 1 ]]; then
-    mkdir -p stdostde
-    NextJob="Genotype gVCFs"
-    QsubCmd="qsub -hold_jid $JOB_ID -o stdostde/ -e stdostde/ $EXOMPPLN/ExmVC.2.GenotypeGVCFs.sh -i $VcfLst -r $RefFil -t $TgtBed -l $LogFil -P"
-    if [[ "$BadET" == "true" ]]; then QsubCmd=$QsubCmd" -B"; fi
-    funcPipeLine
-fi
 #End Log
 funcWriteEndLog

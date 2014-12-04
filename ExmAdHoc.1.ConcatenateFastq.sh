95ee41787b287dfb79abeab985d7a03277886d3c
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-12-04 12:01:13 -0500

    Updated script so that it can be run as an array job and thus concatenate R1 and R2 in two different jobs

--- ExmAdHoc.1.ConcatenateFastq.sh
+++ ExmAdHoc.1.ConcatenateFastq.sh
@@ -5,6 +5,7 @@
 # The main input it is directory containing fastq files
 # All fastq's in the directory will be combined into 1 (single end) or 2 (paired end) files, so the directory should contain fastqs for only 1 sample/readgroup
 # It is necessary to specifiy "SE" for single end or "PE" for paired end
+# If using PE then call the job as an array job using "-t 1-2"
 # If paired end, the files for paired ends should be marked with one of the following: "_R1_" & "_R2_"; ".R1." & ".R2.";"_R1." & "_R2"; "_R1." & "_R2."
 # Generally file names should be in the format such as <SAMPLENAMELANEETCETC>_R<PAIRNUMBER>_<Section_NUMBER>.fastq.gz
 #    FqDir - (required) - A directory containing fastq files
@@ -24,6 +25,8 @@ ExmAdHoc.1.ConcatenateFastq.sh -i <InputDirectory> -t <Type>
      -t (required) - \"PE\" for paired-end \"SE\" for single-end
      -o (optional) - Output filename - if not provided the directory name will be used
      -H (flag) - echo this message and exit
+     
+     NOTE: If using PE then call the job as an array job using \"-t 1-2\"
 "
 
 #get arguments
@@ -60,7 +63,21 @@ if [[ "$Type" == "SE" ]]; then
     echo "$FqFils"
     zcat $FqFils | gzip > $OutNam".fastq.gz"
     echo "Done"
-elif [[ "$Type" == "PE" ]]; then
+elif [[ "$Type" == "PE" ]] && [[ "$SGE_TASK_ID" == "1" ]]; then
+    echo "Paired End - Read 1"
+    FqFils=$(find $FqDir | grep -E "[_.]R1[_.]" | grep "fastq.gz" | uniq | sort)
+    echo "----Fastq List R1 ---"
+    echo "$FqFils"
+    zcat $FqFils | gzip > $OutNam"_R1.fastq.gz"
+    echo "Done R1"
+elif [[ "$Type" == "PE" ]] && [[ "$SGE_TASK_ID" == "2" ]]; then
+    echo "Paired End - Read 2"
+    FqFils=$(find $FqDir | grep -E "[_.]R2[_.]" | grep "fastq.gz" | uniq | sort)
+    echo "----Fastq List R2 ---"
+    echo "$FqFils"
+    zcat $FqFils | gzip > $OutNam"_R2.fastq.gz"
+    echo "Done R2"
+elif [[ "$Type" == "PE" ]] && [[ "$SGE_TASK_ID" == "undefined" ]]; then
     echo "Paired End"
     #if paired end do R1 first then R2
     FqFils=$(find $FqDir | grep -E "[_.]R1[_.]" | grep "fastq.gz" | uniq | sort)

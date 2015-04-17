83d3d9661da4bb1046cebb37f96a5f27375bfa37
Ashley Sawle <ads2202@c2b2.columbia.edu>
2015-04-17 15:20:52 -0400

    Small adjustment to usage info

--- ExmAdHoc.1.ConcatenateFastq.sh
+++ ExmAdHoc.1.ConcatenateFastq.sh
@@ -5,7 +5,7 @@
 # The main input it is directory containing fastq files
 # All fastq's in the directory will be combined into 1 (single end) or 2 (paired end) files, so the directory should contain fastqs for only 1 sample/readgroup
 # It is necessary to specifiy "SE" for single end or "PE" for paired end
-# If using PE then call the job as an array job using "-t 1-2"
+# If using PE then call the job as an array job using "qsub -t 1-2 ...."
 # If paired end, the files for paired ends should be marked with one of the following: "_R1_" & "_R2_"; ".R1." & ".R2.";"_R1." & "_R2"; "_R1." & "_R2."
 # Generally file names should be in the format such as <SAMPLENAMELANEETCETC>_R<PAIRNUMBER>_<Section_NUMBER>.fastq.gz
 #    FqDir - (required) - A directory containing fastq files
@@ -19,14 +19,13 @@
 ###############################################################
 
 usage="
-ExmAdHoc.1.ConcatenateFastq.sh -i <InputDirectory> -t <Type>
+<-t 1-2>* ExmAdHoc.1.ConcatenateFastq.sh -i <InputDirectory> -t <Type> -o <OutputName>
+*NOTE: If using PE then call the job as an array job using \"-t 1-2\"
 
      -i (required) - Path to directory containing fastq.gz files
      -t (required) - \"PE\" for paired-end \"SE\" for single-end
      -o (optional) - Output filename - if not provided the directory name will be used
      -H (flag) - echo this message and exit
-     
-     NOTE: If using PE then call the job as an array job using \"-t 1-2\"
 "
 
 #get arguments

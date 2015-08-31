1c3bff08659b19ec1f5e486b623138c2db4d47ce
Ashley Sawle <ads2202@c2b2.columbia.edu>
2015-08-31 12:36:40 -0400

    Adjusted to handle and output gzipped vcf files. Also, now uses temporary scripts and qsub calls to run the vcftools steps, which speeds up the process.

--- ExmVC.5.MakeKinTestFilesFromVCF.sh
+++ ExmVC.5.MakeKinTestFilesFromVCF.sh
@@ -38,6 +38,7 @@ while getopts i:o:l:H opt; do
 done
 
 #check all required paramaters present
+echo $InpFil
 if [[ ! -e "$InpFil" ]]; then echo "Missing/Incorrect required arguments"; echo "$usage"; exit; fi
 
 #Load script library
@@ -45,12 +46,19 @@ source $EXOMPPLN/exome.lib.sh #library functions begin "func" #library functions
 
 #set local variables
 VcfFil=`readlink -f $InpFil` #resolve absolute path to vcf
-if [[ ! $OutNam ]]; then OutNam=`basename $InpFil | sed s/.vcf// `; fi
+if [[ ! $OutNam ]]; then OutNam=`basename $InpFil | sed s/.gz$// | sed s/.vcf$// | sed s/.hardfiltered$// `; fi
 if [[ ! $LogFil ]];then LogFil=$OutNam.kinshipfiles.log; fi
 TmpLog=Temp.$LogFil
-TmpRscript=Temp.rPlotHist.$OutNam.R
+TmpRscript=Temp.rPlotHist.$OutNam.R # temporary R script to generate sex histogram
+TmpRelatScript=Temp.Relatedeness.$OutNam.sh # temporary bash script to run vcftools relatedness2
+TmpMissingScript=Temp.Missingness.$OutNam.sh # temporary bash script to run vcftools missingness
+mkdir -p stdostde # output directory for std error/output from vcftools bash scripts
+
 SampList=Temp.$OutNam.`date "+%j%y%H%M%N"`.samplist #file to hold list of samples in vcf
 MrgList=Temp.$OutNam.splitplink.merge.list #a file to hold the merge list for plink if it is necessary to convert the vcf in steps
+
+#check the vcf file to see if it is zipped 
+FilTyp=${VcfFil##*.}
 
 #Start Log File
 ProcessName="Generate pedigree analysis files" # Description of the script - used in log
@@ -58,21 +66,34 @@ funcWriteStartLog
 
 #get relatedness via KING algorithm
 StepName="Get relatedness via KING algorithm in vcftools" # Description of this step - used in log
-StepCmd="qsss -c \"vcftools --vcf $VcfFil --relatedness2 --out $OutNam\""
+echo "#!/bin/bash" > $TmpRelatScript
+echo "vcftools --vcf $VcfFil --relatedness2 --out $OutNam" >> $TmpRelatScript
+if [[ $FilTyp == "gz" ]]; then 
+    sed s/--vcf/--gzvcf/g $TmpRelatScript > $TmpRelatScript.2
+    mv -f $TmpRelatScript.2 $TmpRelatScript
+fi
+StepCmd="qsub -o stdostde/ -e stdostde/ -cwd -S /bin/bash -N Relat2 -l mem=8G,time=6:: $TmpRelatScript"
 funcRunStep
 
 #get missingness for each individual
 StepName="Get missingness with vcftools" # Description of this step - used in log
-StepCmd="qsss -c \"vcftools --vcf $VcfFil --missing-indv --out $OutNam.vcf\""
+echo "#!/bin/bash" > $TmpMissingScript
+echo "vcftools --vcf $VcfFil --missing-indv --out $OutNam" >> $TmpMissingScript
+if [[ $FilTyp == "gz" ]]; then 
+    sed s/--vcf/--gzvcf/g $TmpMissingScript > $TmpMissingScript.2
+    mv -f $TmpMissingScript.2 $TmpMissingScript
+fi
+StepCmd="qsub -o stdostde/ -e stdostde/ -cwd -S /bin/bash -N Missing -l mem=8G,time=1:: $TmpMissingScript"
 funcRunStep
 
 #convert to plink - the vcftools vcf --> plink tool cannot run with > 1000 samples on the cluster due to limitations on temporary files. Therefore, for larger cohorts, it is necessary to split and remerge the plink files.
-grep -m 1 "^#CHROM" $VcfFil | cut -f 10- | tr "\t" "\n" > $SampList
+less $VcfFil  | grep -m 1 "^#CHROM" | cut -f 10- | tr "\t" "\n" > $SampList
 LEN=`cat $SampList | wc -l`
 echo "There are $LEN samples in the vcf" >> $TmpLog
 if [[ $LEN -lt 1000 ]]; then
     StepName="Convert vcf to plink ped/map using vcftools"
     StepCmd="vcftools --vcf $VcfFil --plink --out Temp.$OutNam"
+    if [[ $FilTyp == "gz" ]]; then StepCmd=`echo $StepCmd | sed s/--vcf/--gzvcf/g`; fi
     funcRunStep
     StepName="Convert ped/map to bed/bim/fam using plink"
     StepCmd="plink --file Temp.$OutNam --make-bed --out $OutNam"
@@ -84,6 +105,7 @@ else
         NAM=${i##*split.}
         StepName="Convert vcf to plink ped/map using vcftools - step $NAM"
         StepCmd="vcftools --vcf $VcfFil --keep $i --plink --out Temp.$OutNam.splitplink.$NAM"
+        if [[ $FilTyp == "gz" ]]; then StepCmd=`echo $StepCmd | sed s/--vcf/--gzvcf/g`; fi
         funcRunStep
     done
     ls -r | grep splitplink | grep -E "ped$|map$" | paste - - > $MrgList
@@ -129,4 +151,4 @@ funcRunStep
 funcWriteEndLog
 
 #Clean up
-#rm -f Temp.$OutNam.* $OutNam*.sexcheck.* $OutNam*nosex $OutNam.ped $OutNam.map  $OutNam.log $OutNam.lmiss $OutNam*~
+rm -f Temp.$OutNam.* $OutNam*.sexcheck.* $OutNam*nosex $OutNam.ped $OutNam.map  $OutNam.log $OutNam.lmiss $OutNam*~ $TmpRscript
